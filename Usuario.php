1e7ea200d63aa329ade402ec83d87d9c693770d1
Kleber <kleberluanbleodoro@gmail.com>
2026-05-26 16:58:12 -0300

    Classe usuario mvp

--- Usuario.php
+++ Usuario.php
@@ -1,11 +1,32 @@
 <?php
 class Usuario{
+    private ?int $id;
     private string $nome;
     private string $login;
     private string $senha;
 
-    
+    public function __construct(?int $id, string $nome, string $login, string $senha) {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->login = $login;
+        $this->senha = $senha;
+    }
 
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function getLogin(): string {
+        return $this->login;
+    }
+
+    public function getSenha(): string {
+        return $this->senha;
+    }
 }
 
 ?>
