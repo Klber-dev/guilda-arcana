e93051884a95c7322c7f7af85aef932d42a858ca
Kleber <kleberluanbleodoro@gmail.com>
2026-05-25 21:38:46 -0300

    Corrigido nome da classe

---
+++ Database.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database {
+    private $db;
+
+    function __construct($host, $port, $dbname, $username, $password)
+    {   
+        //Instanciamos uma classe PDO no atributo $db, passando os parâmetros de conexao 
+        $this->db = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password );
+
+        //Forma que o pdo vai lidar com os erros
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    //Metodo para retornar a conexao com o banco de dados
+    public function getDb()
+    {
+        return $this->db;
+    }
+}
