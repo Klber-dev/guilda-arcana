43e02c47f515c1c809b2b2af43066b1ea70c0d23
Kleber <kleberluanbleodoro@gmail.com>
2026-05-30 17:04:04 -0300

    refactor

--- Backend/Public/index.php
+++ Backend/Public/index.php
@@ -2,9 +2,8 @@
 
 require_once __DIR__ . '/../Config/autoload.php';
 require_once __DIR__ . '/../Config/headers.php';
-require_once __DIR__ . '/../Config/db_config.php';
+require_once __DIR__ . '/../Config/database_config.php';
 
 $connection = new Database(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS);
 
-
-?>
+session_start();
