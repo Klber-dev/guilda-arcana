7de5ef63f9a4ed6bba2f1a67cd94303cf58142ec
Kleber <kleberluanbleodoro@gmail.com>
2026-05-30 10:09:02 -0300

    Tirando require utilizado pra debbug

--- Backend/Model/UsuarioModel.php
+++ Backend/Model/UsuarioModel.php
@@ -1,7 +1,7 @@
 
 <?php
-require_once "../Database.php";
-require_once "../Usuario.php";
+//require_once "../Database.php";
+// require_once "../Usuario.php";
 
 class UsuarioModel {
     private $db;
