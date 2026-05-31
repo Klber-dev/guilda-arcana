055fd30258ed72d4bbe4d8e5d17df40b3949227a
Kleber <kleberluanbleodoro@gmail.com>
2026-05-31 14:33:48 -0300

    implementando a estrutura inicial do MagoController e método criarMago

--- Backend/Controllers/MagoController.php
+++ Backend/Controllers/MagoController.php
@@ -1,3 +1,22 @@
 <?php
-// Em progresso
-?>
+
+class MagoController extends BaseController {
+    private $magoModel;
+
+    public function __construct(MagoModel $magoModel) {
+        $this->magoModel = $magoModel;
+    }
+
+    public function criarMago(){
+        $data = $this->getJsonInput();
+    
+        if(!isset($data['nome']) || !isset($data['nivel']) || !isset($data['guilda_id'])){
+            $this->sendErrorResponse('Dados incompletos');
+            return;
+        }
+
+        $mago = new Mago(null, $data['nome'], $data['nivel'], $data['guilda_id']);
+
+    }
+
+}
