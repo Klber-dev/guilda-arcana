bef6417f303eb1238e3b7d6cbfe4e26c820143ad
Kleber <kleberluanbleodoro@gmail.com>
2026-05-31 17:43:23 -0300

    BACKEND FEITO

--- Backend/Public/index.php
+++ Backend/Public/index.php
@@ -1,9 +1,70 @@
 <?php
 
-require_once __DIR__ . '/../Config/autoload.php';
+session_start();
+
 require_once __DIR__ . '/../Config/headers.php';
+require_once __DIR__ . '/../Config/autoload.php';
 require_once __DIR__ . '/../Config/database_config.php';
 
-$connection = new Database(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS);
+$db = new Database(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS);
 
-session_start();
+$rota = $_GET['rota'] ?? '';
+$acao = $_GET['acao'] ?? '';
+$_SESSION['usuario_id'] = 1;
+
+switch ($rota) {
+    case 'usuarios':
+        $controller = new UsuarioController($db);
+
+        if ($acao === 'cadastrar') {
+            $controller->cadastrarUsuario();
+        } elseif ($acao === 'login') {
+            $controller->autenticarUsuario();
+        } elseif ($acao === 'atualizar') {
+            $controller->atualizarPerfil();
+        } elseif ($acao === 'excluir') {
+            $controller->excluirUsuario();
+        }
+        break;
+
+    case 'guildas':
+        $controller = new GuildaController($db);
+
+        if ($acao === 'criar') {
+            $controller->criarGuilda();
+        } elseif ($acao === 'buscar') {
+            $controller->getGuilda();
+        } elseif ($acao === 'atualizar') {
+            $controller->atualizarGuilda();
+        } elseif ($acao === 'excluir') {
+            $controller->excluirGuilda();
+        }
+        break;
+
+    case 'magos':
+        $controller = new MagoController($db);
+
+        if ($acao === 'criar') {
+            $controller->criarMago();
+        } elseif ($acao === 'listar') {
+            $controller->getMagos();
+        } elseif ($acao === 'apagar') {
+            $controller->apagarMago();
+        }
+        break;
+
+    case 'mago-magias':
+        $controller = new MagoMagiaController($db);
+
+        if ($acao === 'aprender') {
+            $controller->aprenderMagia();
+        } elseif ($acao === 'desaprender') {
+            $controller->desaprenderMagia();
+        } elseif ($acao === 'listar') {
+            $controller->listarMagiasdoMago();
+        }
+        break;
+
+    default:
+        echo json_encode(['error' => 'Rota não encontrada']);
+}
