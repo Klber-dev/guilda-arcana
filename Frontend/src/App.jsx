a463dcbb69bd139ec98b0e9de7e25f24acc36724
Kleber <kleberluanbleodoro@gmail.com>
2026-06-01 16:49:30 -0300

    add roteamento

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,32 @@
 import { Routes, Route } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import PerfilUsuario from "./pages/PerfilUsuario";
+import PerfilGuilda from "./pages/PerfilGuilda";
+import CadastroMago from "./pages/CadastroMago";
+import Spellbook from "./pages/Spellbook";
+import Sobre from "./pages/Sobre";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/cadastro" element={<Cadastro />} />
+
+      <Route path="/perfil" element={<PerfilUsuario />} />
+
+      <Route path="/guilda" element={<PerfilGuilda />} />
+
+      <Route path="/magos/cadastrar" element={<CadastroMago />} />
+
+      <Route path="/spellbook" element={<Spellbook />} />
+      
+      <Route path="/sobre" element={<Sobre />} />
     </Routes>
   );
 }
