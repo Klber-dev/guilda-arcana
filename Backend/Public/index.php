706b30e882db5d9bed391143891b8ce827254099
Kleber <kleberluanbleodoro@gmail.com>
2026-06-01 17:30:05 -0300

    ajuste nos headers

--- Backend/Public/index.php
+++ Backend/Public/index.php
@@ -1,10 +1,10 @@
 <?php
 
-session_start();
-
 require_once __DIR__ . '/../Config/headers.php';
 require_once __DIR__ . '/../Config/autoload.php';
 require_once __DIR__ . '/../Config/database_config.php';
+session_start();
+
 
 $db = new Database(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS);
 
