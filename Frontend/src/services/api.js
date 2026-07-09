60a2f1c73f6739dfff976a18580a10b133532eb7
Kleber <kleberluanbleodoro@gmail.com>
2026-07-08 22:45:25 -0300

    fix: URL do backend corrigida

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/Public/index.php',
+    baseURL: 'http://localhost:8080/index.php',
     withCredentials: true,
 });
 
